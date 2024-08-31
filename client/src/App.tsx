c91bf90fd3820977e1039f6768e1b7d3fd534d04
KartikChawla09 <kartik.chawla.ug21@nsut.ac.in>
2024-08-31 18:15:52 +0530

    established socket connection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,25 @@
-import { Suspense } from "react";
+import React, { Suspense, useEffect } from "react";
 import { HashRouter } from "react-router-dom";
 import { Router } from "@/router";
 import { ToastContainer } from "react-toastify";
 import { AuthProvider } from "@/hooks/useAuth";
 import { ChakraProvider } from "@chakra-ui/react";
+import { socket } from "./hooks/socket";
 
 import "react-toastify/dist/ReactToastify.css";
 import "@/assets/css/index.css";
 
 export const App = () => {
+  useEffect(() => {
+    socket.connect();
+    socket.on("connect", () => {
+      console.log("Socket.IO connected successfully!");
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <HashRouter>
       <AuthProvider>
