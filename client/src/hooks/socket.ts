27c45155a97870d7359a48e60e8989c0c037e6c9
Rakshit Verma <rakshit.verma0911@gmail.com>
2024-09-01 23:42:51 +0530

    localhost variables replaced

--- client/src/hooks/socket.ts
+++ client/src/hooks/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const SOCKET_URL = "http://localhost:8000";
+const SOCKET_URL = "https://sih2024-r1-3.onrender.com";
 export const socket = io(SOCKET_URL, {
   transports: ["websocket"],
   autoConnect: false,
