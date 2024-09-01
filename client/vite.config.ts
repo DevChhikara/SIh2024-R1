a794c7f2d0b1b2a0fc101ee4b027f5493fa4f031
Dev Chhikara <devchhikara@Devs-MacBook-Air-2.local>
2024-09-01 19:57:27 +0530

    env example

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src/"),
     },
   },
-  base: process.env.NODE_ENV === "production" ? "/google-sheets" : "/",
+  base: process.env.NODE_ENV === "production" ? "/sheets" : "/",
   server: { port: 3000 },
 });
