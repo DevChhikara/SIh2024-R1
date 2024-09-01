0e8c7e69c9dc14c84e97aff7cafb98703f112e8a
Dev Chhikara <dvchhikara36@gmail.com>
2024-09-01 21:04:08 +0530

    cors1

--- server/src/middlewares/cors.ts
+++ server/src/middlewares/cors.ts
@@ -2,24 +2,29 @@ import { Request, Response, NextFunction } from "express";
 
 type CORS = (req: Request, res: Response, next: NextFunction) => void;
 
-
-let allowedHeaders = ["Authorization", "Content-Type"];
+const allowedHeaders = ["Authorization", "Content-Type", "sec-ch-ua", "sec-ch-ua-mobile", "sec-ch-ua-platform"];
+const allowedOrigins = ["https://s-ih2024-r1-8uq7.vercel.app"];
 
 const cors: CORS = (req, res, next) => {
-  let origin = req.headers.origin;
-  let method = req.method;
+  const origin = req.headers.origin;
+  const method = req.method;
 
-  if (origin) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "*");
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", allowedHeaders.join(", "));
     res.setHeader("Access-Control-Allow-Credentials", "true");
 
-    return method == "OPTIONS" ? res.status(200).end() : next();
+    if (method === "OPTIONS") {
+      return res.status(200).end();
+    } else {
+      return next();
+    }
+  } else if (!origin) {
+    return next();
+  } else {
+    return res.status(403).end();
   }
-
-  return !origin ? next() : res.status(403).end();
 };
-
 
 export default cors;
