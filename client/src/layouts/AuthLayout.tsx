61fde3947fd39a94c1690b71347ef6637158e9ca
Dev Chhikara <devchhikara@Devs-MacBook-Air-2.local>
2024-09-01 05:34:12 +0530

    signUp/SignIn updated

--- client/src/layouts/AuthLayout.tsx
+++ client/src/layouts/AuthLayout.tsx
@@ -1,8 +1,9 @@
 import { Outlet } from "react-router-dom";
+import './styles.css';
 
 const AuthLayout = () => {
   return (
-    <div className="flex items-center justify-center min-h-full bg-[#e3edfd]">
+    <div className="flex items-center justify-center min-h-full">
       <Outlet />
     </div>
   );
